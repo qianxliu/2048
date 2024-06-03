abbf83993e9df48a2339c8e8c920bf206275dca1
dreamcatcher <qianxliu>
2024-06-03 18:04:58 +0800

    still need to handle infinity loops, and this doesn't manage the sub problems

--- 2048.cpp
+++ 2048.cpp
@@ -22,96 +22,106 @@ bool addRandomTile() {
         }  
     }  
 }  
-  
-vector<vector<int*>> columns;
-vector<vector<int*>> rows;
-void initializeGrid() {  
+
+void initialize() {  
     addRandomTile();  
     addRandomTile();  
+}
+
+void msg(int &i)
+{
+    if (i == 2048)
+        std::cout << "You reached 2048!" << '\n';  
 }
 
 int score = 0;
 
 void mergeUp() {  
-    for (int col = 0; col < SIZE; ++col) {  
-        std::vector<int*> column; 
-        for (int row = 0; row < SIZE; ++row) {  
-            if (grid[row][col] != 0)
-                column.push_back(&grid[row][col]);  
-        }
-
-        // 合并temp中的数字  
-        int idx = 0;  
-        while (column.size() > 1 && idx < column.size() - 1) {  
-            if (*column[idx] == *column[idx + 1]) {  
-                *column[idx] *= 2; // 合并相同的数字
-                score += *column[idx];  
-                column.erase(column.begin() + idx + 1); // 移除已合并的数字  
-  
-                // 如果合并后的数字等于2048，输出消息并结束循环  
-                if (*column[idx] == 2048) {  
-                    std::cout << "You reached 2048!" << std::endl;  
-                    break; // 跳出循环，因为我们只关心2048这个数字是否出现  
-                }  
-            }  
-            ++idx;  
+    std::vector<int*> column;   
+    for (int col = 0; col < SIZE; ++col) {    
+        column.clear(); // 清除上一列的指针  
+        for (int row = 0; row < SIZE; ++row) {    
+            if (grid[row][col] != 0)  
+                column.push_back(&grid[row][col]);    
         }  
-        
-        idx = 0;
-        // 将合并后的数字放回newGrid中  
-        for (int row = 0; row < SIZE; ++row) {  
-            if (idx < column.size())    grid[row][col] = *column[idx++];  
-            else grid[row][col] = 0;
+    
+        // 如果列中没有数字，则跳过  
+        if (column.empty()) continue;  
+
+        int idx = 0;    
+        while (idx < column.size() - 1) {    
+            if (*column[idx] == *column[idx + 1]) {    
+                *column[idx] *= 2; // 合并相同的数字  
+                score += *column[idx];    
+                // 注意：删除下一个元素后，不需要再次增加idx，因为下一个元素会移动到当前位置  
+                column.erase(column.begin() + idx + 1); // 移除已合并的数字    
+
+                // 如果合并后的数字等于2048，输出消息  
+                msg(*column[idx]);  
+            } else ++idx;  
+        }  
+    
+        // 将合并后的数字放回grid中  
+        idx = 0;  
+        for (int row = 0; row < SIZE; ++row) {    
+            if (idx < column.size()) {  
+                grid[row][col] = *column[idx++]; // 只在column中有值时赋值，否则为0  
+            } else {  
+                grid[row][col] = 0; // 如果column中的值都用完了，则剩余的行都为0  
+            }  
         }  
     }
+
 }  
 
 void mergeLeft() {  
+    std::vector<int*> rows; 
     for (int row = 0; row < SIZE; ++row) {  
-        std::vector<int*> rows; 
+        rows.clear();
         for (int col = 0; col < SIZE; ++col) {  
             if (grid[row][col] != 0)
                 rows.push_back(&grid[row][col]);  
         }
 
+    // 如果列中没有数字，则跳过  
+    if (rows.empty()) continue;  
         // 合并temp中的数字  
         int idx = 0;  
-        while (rows.size() > 1 && idx < rows.size() - 1) {  
+        while (idx < rows.size() - 1) {  
             if (*rows[idx] == *rows[idx + 1]) {  
                 *rows[idx] *= 2; // 合并相同的数字  
-                                score += *rows[idx];  
+                score += *rows[idx];  
 
                 rows.erase(rows.begin() + idx + 1); // 移除已合并的数字  
   
                 // 如果合并后的数字等于2048，输出消息并结束循环  
-                if (*rows[idx] == 2048) {  
-                    std::cout << "You reached 2048!" << std::endl;  
-                    break; // 跳出循环，因为我们只关心2048这个数字是否出现  
-                }  
-            }  
-            ++idx;  
+                msg(*rows[idx]);
+            }  else ++idx;  
         }  
 
         idx = 0;
         // 将合并后的数字放回newGrid中  
         for (int col = 0; col < SIZE; ++col) {  
-            if (idx < rows.size())    grid[row][col] = *rows[idx++];  
+            if (idx < rows.size())    grid[row][col] = *rows[idx++];
             else grid[row][col] = 0;
         }  
     }
 }  
 
-void mergeDown() {  
+void mergeDown() {
+    vector<int*> column;
     for (int col = 0; col < SIZE; ++col) {  
-        std::vector<int*> column; 
+        column.clear();
         for (int row = SIZE-1; row >= 0; --row) {  
             if (grid[row][col] != 0)
                 column.push_back(&grid[row][col]);  
         }
 
+    if (column.empty()) continue;  
+
         // 合并temp中的数字  
         int idx = 0;  
-        while (column.size() > 1 && idx < column.size() - 1) {  
+        while (idx < column.size() - 1) {  
             if (*column[idx] == *column[idx + 1]) {  
                 *column[idx] *= 2; // 合并相同的数字  
                                 score += *column[idx];  
@@ -119,34 +129,33 @@ void mergeDown() {
                 column.erase(column.begin() + idx + 1); // 移除已合并的数字  
   
                 // 如果合并后的数字等于2048，输出消息并结束循环  
-                if (*column[idx] == 2048) {  
-                    std::cout << "You reached 2048!" << std::endl;  
-                    break; // 跳出循环，因为我们只关心2048这个数字是否出现  
-                }  
-            }  
-            ++idx;  
+                msg(*column[idx]);
+
+            }  else ++idx;  
         }
 
         idx = 0;
         // 将合并后的数字放回newGrid中  
         for (int row = SIZE-1; row >= 0; --row) {  
-            if (idx < column.size())    grid[row][col] = *column[idx++];  
+            if (idx < column.size())    grid[row][col] = *column[idx++];              
             else grid[row][col] = 0;
         }  
     }
 }  
 
 void mergeRight() {  
+    vector<int*> rows;
     for (int row = 0; row < SIZE; ++row) {  
-        std::vector<int*> rows; 
-        for (int col = SIZE-1; col >=0; ++col) {  
+        rows.clear();
+        for (int col = SIZE-1; col >=0; --col) {  
             if (grid[row][col] != 0)
                 rows.push_back(&grid[row][col]);  
         }
 
+    if (rows.empty()) continue;  
         // 合并temp中的数字  
         int idx = 0;  
-        while (rows.size() > 1 && idx < rows.size() - 1) {  
+        while (idx < rows.size() - 1) {  
             if (*rows[idx] == *rows[idx + 1]) {  
                 *rows[idx] *= 2; // 合并相同的数字  
                                 score += *rows[idx];  
@@ -154,49 +163,53 @@ void mergeRight() {
                 rows.erase(rows.begin() + idx + 1); // 移除已合并的数字  
   
                 // 如果合并后的数字等于2048，输出消息并结束循环  
-                if (*rows[idx] == 2048) {  
-                    std::cout << "You reached 2048!" << std::endl;  
-                    break; // 跳出循环，因为我们只关心2048这个数字是否出现  
-                }  
-            }  
-            ++idx;  
+                msg(*rows[idx]);
+
+            }  else ++idx;  
         }  
 
         idx = 0;
         // 将合并后的数字放回newGrid中  
         for (int col = SIZE-1; col >= 0; --col) {  
-            if (idx < rows.size())    grid[row][col] = *rows[idx++];  
+            if (idx < rows.size())    grid[row][col] = *rows[idx++]; // 只在column中有值时赋值，否则为0  
             else grid[row][col] = 0;
         }  
     }
 }  
 
 
-void merge(char direction) {
+bool check(char direction) {
     switch (direction)
     {
-        case 'w':
-        case 'W':
+		case 119: // 'w' key
+		case 107: // 'k' key
+		case 65:  // up arrow
             mergeUp();
             break;
-        case 'a':
-        case 'A':
-                    mergeLeft();
+		case 97:  // 'a' key
+		case 104: // 'h' key
+		case 68:  // left arrow
+            mergeLeft();
                     break;
-        case 's':
-        case 'S':
-                    mergeDown();
+		case 115: // 's' key
+		case 106: // 'j' key
+		case 66:  // down arrow
+                mergeDown();
                     break;
-        case 'd':
-        case 'D':
-                    mergeRight();
+		case 100: // 'd' key
+		case 108: // 'l' key
+		case 67:  // right arrow
+                mergeRight();
                     break;
+        default:
+            return false;
     }
+    return true;
 }  
   
 void refresh() {
     cout << '\n'; 
-    system("clear");
+    printf("\033[H"); // move cursor to 0,0
     for (int row = 0; row < SIZE; ++row) {  
         for (int col = 0; col < SIZE; ++col) {  
             std::cout << std::setw(3) << grid[row][col] << " ";  
@@ -210,29 +223,37 @@ void refresh() {
 #include <unistd.h>
 
 int main() {  
-    std::cout << "Enter move (w/a/s/d or q/enter to quit): ";  
-    initializeGrid();  
-    refresh();  
+    initialize();  
+    std::cout << "Enter move (w/a/s/d or q/enter to quit): \n";  
 
+	// make cursor invisible, erase entire screen
+    printf("\033[?25l\033[2J");
     struct termios oldt, newt;  
-  
     tcgetattr(STDIN_FILENO, &oldt); // 保存旧的终端设置  
     newt = oldt;  
-    newt.c_lflag &= ~(ICANON | ECHO); // 关闭规范模式和回显  
+    newt.c_lflag &= (~ICANON & ~ECHO); // 关闭规范模式和回显, disable canonical mode (buffered i/o) and local echo
     tcsetattr(STDIN_FILENO, TCSANOW, &newt); // 应用新的终端设置  
 
+    refresh();
     char direction;  
     while ((direction = getchar()) != ('\n')) {  
-            refresh(); 
             if (direction == 'q') {  
                 break;  
             }
-            merge(direction);  
-            addRandomTile();
-
+            refresh();
+            if (check(direction))
+            {  
+                refresh(); 
+                addRandomTile();
+                refresh();
+            } 
         // TODO: 检查游戏是否结束（无法再合并）  
     }  
-  
+  	
+
     tcsetattr(STDIN_FILENO, TCSANOW, &oldt); // 恢复旧的终端设置  
+    // make cursor visible, reset all modes
+	printf("\033[?25h\033[m");
+
     return 0;  
 }
